6d39927ae8a1cd8e356d639e463d7d8571190bb4
StivlWrith <32070483+StivlWrith@users.noreply.github.com>
2017-12-06 02:45:02 +0100

    numerobis

--- EDDI/EDDI.cs
+++ EDDI/EDDI.cs
@@ -215,7 +215,7 @@ namespace Eddi
                 }
                 else
                 {
-                    // If InvokeUpdatePlugin failed then it will have have left an error message, but this once we ignore it
+                    // If InvokeUpdatePlugin failed then it will have have left an error message, but this once we ignore it.
                     Logging.Info("EDDI access to the companion app is disabled");
                 }
 
